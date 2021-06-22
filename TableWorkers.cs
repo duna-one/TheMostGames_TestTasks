931b4d4f6f7f9f6f3893896b33bf11519a3632e5
duna-one <korotkovdan@gmail.com>
2021-06-22 11:35:04 +0300

    Created TableWorkers class (for datagrid)

---
+++ TableWorkers.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_1
+{
+    class TableWorkers
+    {
+        public string Text { get; set; }
+        public uint WordsCount { get; set; }
+        public uint VowelsCount { get; set; }
+
+        TableWorkers(string text)
+        {
+            Text = text;
+            WordsCount = WordCounter();
+            VowelsCount = VolwesCounter();
+        }
+
+        private uint WordCounter()
+        {
+            return (uint)Text.Split(" ").Length;
+        }
+
+        private uint VolwesCounter()
+        {
+            Vowels vowels = new Vowels();
+            uint counter = 0;
+            foreach (char letter in Text)
+            {
+                if (vowels.isVowel(letter))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
