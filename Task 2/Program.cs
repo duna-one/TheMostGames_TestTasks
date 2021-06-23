baca7ec6d36cc195732626714d9eabe6dacba349
Даниил <korotkovdan@outlook.com>
2021-06-23 17:58:40 +0300

    Startet work on Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        private static string fileSrc;
+
+        private static void Main(string[] args)
+        {
+           
+
+        }
+    }
+}
