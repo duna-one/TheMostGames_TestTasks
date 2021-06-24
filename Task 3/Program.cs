de686f94f041e991dc1909af1af3f93ded6ac90a
Даниил <korotkovdan@outlook.com>
2021-06-24 16:39:50 +0300

    Task 3 starts

---
+++ Task 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
