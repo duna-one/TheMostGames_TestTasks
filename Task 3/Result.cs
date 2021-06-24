d3f383119723aaa491a1029d0fe082459a165373
Даниил <korotkovdan@outlook.com>
2021-06-24 18:12:23 +0300

    A class describing the result of the program was created

---
+++ Task 3/Result.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Task_3
+{
+    internal class Result
+    {
+        public string RussianString { get; private set; }
+        public List<string> EnglishStrings { get; private set; }
+
+        public Result(string rusString)
+        {
+            RussianString = rusString;
+            EnglishStrings = new List<string>();
+        }
+
+        public void AddEnglishString(string engString)
+        {
+            EnglishStrings.Add(engString);
+        }
+
+        public override string ToString()
+        {
+            string output = "Russian string:\n" + RussianString + "\n";
+            foreach (string str in EnglishStrings)
+            {
+                output += str + "\n";
+            }
+            return output;
+        }
+    }
+}
